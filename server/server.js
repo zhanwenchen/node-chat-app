c8d74ecb2452ec473383251e76f33d35ed8936ab
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2016-10-27 08:49:40 -0400

    added socket interactions

--- server/server.js
+++ server/server.js
@@ -11,8 +11,12 @@ var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-io.on('connection', () => {
+io.on('connection', (socket) => {
   console.log('New user connected');
+
+  socket.on('disconnect', () => {
+    console.log('User disconnected');
+  })
 });
 
 server.listen(port, () => {
