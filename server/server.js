001712f4d834ffad61fc8871c92a3942f608ca0c
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2016-10-27 08:33:43 -0400

    included socket.io

--- server/server.js
+++ server/server.js
@@ -1,13 +1,20 @@
-const PORT = process.env.PORT || 3000;
-
 const path = require('path');
-const publicPath = path.join(__dirname, '../public');
+const http = require('http');
+const express = require('express');
+const socketIO = require('socket.io');
 
-var express = require('express');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 3000;
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
 
 app.use(express.static(publicPath));
 
-app.listen(PORT, () => {
-  console.log('Server started on port ' + PORT);
+io.on('connection', () => {
+  console.log('New user connected');
+});
+
+server.listen(port, () => {
+  console.log(`Server started on ${port}`);
 });
