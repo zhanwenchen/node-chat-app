64e5cc2de00cba46b4e1d8b1871278bdf61551d7
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2016-10-28 12:56:56 -0400

    implemented tests for geolocation

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -11,5 +11,19 @@ describe('generateMessage', () => {
     // asset from matches value passed in
     expect(message.createdTimestamp).toBeA('number');
     expect(message).toInclude({from, text});
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct locationMessage object', () => {
+    // store assertion in response
+    var from = 'Phil';
+    var longitude = '41.700370788574';
+    var latitude = '-73.920967102051';
+    var url = `https://www.google.com/maps?q=${longitude},${latitude}`;
+    var message = generateLocationMessage(from, longitude, latitude);
+    // asset from matches value passed in
+    expect(message.createdTimestamp).toBeA('number');
+    expect(message).toInclude({from, url});
   });
 });
