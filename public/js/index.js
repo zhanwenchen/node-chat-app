0577ac818fb66e62a11c4615eafa90d286d81599
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2016-10-28 09:49:27 -0400

    Implemented jQuery DOM manipulation

--- public/js/index.js
+++ public/js/index.js
@@ -10,4 +10,27 @@ socket.on('disconnect', function() {
 
 socket.on('newMessage', function(message) {
     console.log('newMessage', message);
+    var li = jQuery('<li></li>');
+    li.text(`${message.from}: ${message.text}`);
+
+    jQuery('#messages').append(li);
+});
+
+socket.emit('createMessage', {
+    from: 'Frank',
+    text: 'Hi'
+}, function (data) {
+    console.log('Server received message ', data);
+});
+
+jQuery('#message-form').on('submit', function (e) {
+    // Prevent submit's default refresh
+    e.preventDefault();
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: jQuery('[name=message]').val()
+    }, function () {
+
+    });
 });
