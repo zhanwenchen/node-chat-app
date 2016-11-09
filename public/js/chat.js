8a8a44d5971d050b151db79b42bf97384da668a1
Zhanwen Chen <phil.zhanwen.chen@gmail.com>
2016-11-09 15:09:25 -0500

    closed html tags

--- public/js/chat.js
+++ public/js/chat.js
@@ -18,7 +18,16 @@ function scrollToBottom () {
 }
 
 socket.on('connect', function() {
-    console.log('Connected to server');
+    // console.log('Connected to server');
+    var params = jQuery.deparam(window.location.search);
+
+    socket.emit('join', params, function (err) {
+      if (err) {
+
+      } else {
+        
+      }
+    });
 });
 
 socket.on('disconnect', function() {
